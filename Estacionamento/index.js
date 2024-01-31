ee4d4d2de292902ad8f0d30b7bb68d0ead50f77e
Pedro Melo <pedromelo55@gmail.com>
2024-01-31 00:05:40 -0300

    Conexão com servidor

--- Estacionamento/index.js
+++ Estacionamento/index.js
@@ -22,9 +22,9 @@ app.get("/sensor", function(req, res) {
 app.get("/updateVaga/:id", async(req, res) => {
     try {
         const id = req.params.id;
-        const vaga = await axios.get(`http://localhost:3000/slots/${id}`);
+        const vaga = await axios.get(`http://198.211.115.152:3003/slots/${id}`);
         console.log("Vaga", vaga.data);
-        const vagas = await axios.put(`http://localhost:3000/slots/${id}`, {
+        const vagas = await axios.put(`http://198.211.115.152:3003/slots/${id}`, {
             ...vaga.data, occupied:!vaga.data.occupied
         });
         console.log("Vagas", vagas.data)
@@ -37,7 +37,7 @@ app.get("/updateVaga/:id", async(req, res) => {
 
 app.get("/vagas", async(req, res) => {
     try {
-        const vagas = await axios.get("http://localhost:3000/slots");
+        const vagas = await axios.get("http://198.211.115.152:3003/slots");
         res.render("vagas", {vagas:vagas.data});
     } catch (error) {
         console.error(error);
@@ -47,7 +47,7 @@ app.get("/vagas", async(req, res) => {
 
 app.get("/loadVagas", async (req, res) => {
     try {
-      const vagas = await axios.get("http://localhost:3000/slots");
+      const vagas = await axios.get("http://198.211.115.152:3003/slots");
       res.json(vagas.data); // Remova o .send() antes do .json()
     } catch (error) {
       console.error(error);
